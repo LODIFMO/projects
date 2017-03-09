b4e0f131bdef897efcb0d3f7790783b975b1620b
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2017-03-09 17:09:12 +0300

    :heavy_plus_sign: add assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require bootstrap-sprockets
+//= require vue
+//= require vue-router (optional)
+//= require vue-resource (optional)
+//= require vuex (optional)
 //= require_tree .
