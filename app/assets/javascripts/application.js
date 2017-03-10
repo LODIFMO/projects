0ba7568808a299a418f4d748f1fef0402ea9c425
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2017-03-10 11:19:13 +0300

    :bug: fix require for js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require vue
-//= require vue-router (optional)
-//= require vue-resource (optional)
-//= require vuex (optional)
+//= require vue-router
+//= require vue-resource
+//= require vuex
 //= require_tree .
