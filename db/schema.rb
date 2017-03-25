e4ce39aae83f9aa6c06491eee1800e0ab715bf00
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2017-03-25 16:54:03 +0300

    :wrench: fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309174703) do
+ActiveRecord::Schema.define(version: 20170325092012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
-    t.string   "uri"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "code"
+    t.string   "owner"
+    t.string   "repo_url"
+    t.string   "wiki_url"
   end
 
 end
